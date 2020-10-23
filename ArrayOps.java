204f2b998a92197894c8a2a02570475943d92cb4
Logan <lruzzier20@stuy.edu>
2020-10-23 12:08:42 -0400

    added sum() and test case

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,5 +1,9 @@
 public class ArrayOps{
-  public static void main(String[] args){
-
+  public static int sum(int[] givenArray){
+      int total = 0;
+      for(int i=0;i<givenArray.length;i++){
+        total = total + givenArray[i];
+      }
+      return total;
   }
 }
--- Tester.java
+++ Tester.java
@@ -1,5 +1,6 @@
 public class Tester{
   public static void main(String[] args){
-
+    int[] a = new int[]{1,2,3,4,5,6};
+    System.out.println(ArrayOps.sum(a));
   }
 }
