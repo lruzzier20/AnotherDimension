e71f664d11d5bc61f56652082ebd1c9b845048f5
Logan <lruzzier20@stuy.edu>
2020-10-23 12:13:08 -0400

    added largest() and test case

--- ArrayOps.java
+++ ArrayOps.java
@@ -6,4 +6,12 @@ public class ArrayOps{
       }
       return total;
   }
+
+  public static int largest(int[] givenArray){
+    int largest = 0;
+    for(int i=0;i<givenArray.length;i++){
+      if(givenArray[i]>largest){largest=givenArray[i];}
+    }
+    return largest;
+  }
 }
